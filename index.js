dc6f06e8373d0a33a49fbc192f669e7770b96bb5
Mei Kristian Laoli <laolikristian1305@gmail.com>
2023-08-26 21:30:39 +0700

    up

--- index.js
+++ index.js
@@ -1,25 +1,63 @@
 const express = require("express");
 const app = express();
+const PORT = 5000;
+const path = require("path");
 
-app.get("/Hello World", (req, res) => {
-  res.send("Hello World");
+// set up call hbs untuk sub folder
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "src/views"));
+
+// set serving static file berupa file yang akan di tampilin untuk image dan js yang diambil dari file assets
+app.use(express.static("src/assets"));
+
+// routing
+app.get("/index", home);
+app.get("/blog", blog);
+app.get("/contact", contact);
+app.get("/blog-detail/:id", blogDetail);
+
+// local server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
-app.get("/Cinta", (req, res) => {
-  res.send({
-    id: 1,
-    name: "Kris",
-  });
-});
+// form home
+function home(req, res) {
+  res.render("index");
+}
 
-app.get("/Love", (req, res) => {
-  res.send("testing");
-});
+// form blog
+function blog(req, res) {
+  res.render("blog");
+}
 
-app.get("/Do You Love Me", (req, res) => {
-  res.send("Do");
-});
+// form contact
+function contact(req, res) {
+  res.render("contact");
+}
 
-app.listen(5000, () => {
-  console.log("Example app listening on port 5000");
-});
+function blogDetail(req, res) {
+  const { id } = req.params;
+  //   console.log(id);
+
+  const data = {
+    id,
+    title: "SpongeBob adalah spons laut eksentrik",
+    content:
+      "Perkenalkan saya kris, saya membuat web ini untuk mengembangkan skill dalam belajar berkarir sebagai.",
+  };
+
+  res.render("blog-detail", data);
+}
+
+// const data = {
+//     id: 1,
+//     name: "jhon"
+// }
+
+// console.log(data.id)
+
+// const { id, name } = data
+
+// console.log(id)
+// console.log(name)
